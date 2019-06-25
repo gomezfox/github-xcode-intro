e1466e4614dba64144e77e20ede442ff4d4fbe1d
Brian Bansenauer <brianb@cascadia.edu>
2019-06-25 14:43:30 -0700

    Best feature ever!!!

--- GettingStartedFollowAlong/ViewController.swift
+++ GettingStartedFollowAlong/ViewController.swift
@@ -5,6 +5,10 @@
 //  Created by Michael Panitz on 6/17/19.
 //  Copyright © 2019 Michael Panitz. All rights reserved.
 //
+// CHANGE
+// THIS IS MY NEW CHANGE
+
+
 
 import UIKit
 
@@ -12,6 +16,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
 
